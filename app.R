92f1bc56ed2df55c6afd70d5206faa0022715ffe
Vicent Boned - Home <vicent.boned.11@gmail.com>
2020-05-28 23:57:38 +0200

    Re-organizing code
    * Changed from shinydasbhoard to simple shiny
    * Changed sidebarlayout to navpanel
    * Deleted complex file structure - >not needed for simple app

--- app.R
+++ app.R
@@ -1,61 +1,42 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+source("./auxiliary_files/auxiliary.R")
 
-
-ups <- expand.grid(c(3:45), c(2:9))
-out <- list()
-for(ii in 1:nrow(ups)){
-    out[[ii]] <- combi(ups[ii,1], ups[ii,2])
-}
-out
-
-
-
-combi <- function(value, cells){
-    x    <- combn(c(1:9), cells)
-    x2 <- colSums(x)
-    possible_combinations <- t(x[,which(x2 == value)])
-    return(possible_combinations)
-}
-
-library(shiny)
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            numericInput("totalsum",
-                        "Number to decompose:",
-                      value  =1),
-            numericInput("partitions",
-                      "Number of cells",
-                      value =1)
+    theme = shinytheme("sandstone"),
+    titlePanel("Killer sudoku Helper"),
+    navlistPanel(
+        tabPanel("What is killer Sudoku"),
+        tabPanel("Unique combinations"),
+        tabPanel("1 Group helper",
+            fluidRow(
+                    box(width = 4 ,
+                        numericInput(inputId = "totValue",
+                                     label = NULL, value = 1)), 
+                    box(width = 4, 
+                        numericInput(inputId = "cells",
+                                     label = NULL, value = 1)),
+                    box(width = 4,
+                        reactableOutput("combinations"))
+                )
             ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           tableOutput("combinations")
-        )
-    )
+        tabPanel("2 Groups Helper"))
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    output$combinations_summary <- renderReactable({
+       reactable(combinations_summary %>%
+            select("Number of Cells" = cells,
+                   "Value of Sum" = totValue,
+                   "Number of Combinations" = combinations))
+        })
 
-    output$combinations <- renderTable({
-        combi(input$totalsum, input$partitions)
-    })
+    output$combinations <- renderReactable({
+        aux <- combi(input$totValue, input$cells)
+        reactable(aux)}
+        )
+    
 }
 
 # Run the application 
---
+++ auxiliary_files/auxiliary.R
@@ -0,0 +1,42 @@
+rm(list = ls())
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(reactable)
+library(shinythemes)
+
+combi <- function(value, cells){
+  x    <- combn(c(1:9), cells)
+  x2 <- colSums(x)
+  possible_combinations <- t(x[,which(x2 == value)])
+  return(possible_combinations)
+}
+
+possible_combinations <- expand.grid(totValue = c(3:45), cells =  c(2:9)) %>%
+  unite(key, totValue, cells, remove = F, sep="")
+
+combinations <- apply(possible_combinations[,c("totValue", "cells")], 1, 
+                      function(x) combi(x[1], x[2]))
+names(combinations) <- possible_combinations$key
+
+valid_combinations <- Filter(function(x) nrow(x) > 0 ,combinations)
+amount_of_combinations <- enframe(sapply(valid_combinations, nrow),
+                                  name = "key", value = "combinations")
+
+combinations_summary <- amount_of_combinations %>% 
+  left_join(possible_combinations, by = "key")
+
+ 
+ # reactable(combinations_summary[,c("totValue","cells")],
+ #           columns = list(
+ #             totValue = colDef(name = "Sum Value"),
+ #             cells = colDef(name = "Cells",
+ #                            filterable = F)),
+ #           bordered =TRUE,
+ #           filterable = TRUE,
+ #           minRows = 10,
+ #           groupBy = "totValue")
+
+
+
+
